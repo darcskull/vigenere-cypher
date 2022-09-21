72cb9f6a7de3d7c148ac3c50dc5c9e61b9e501d1
Martin Hristov <74406901+darcskull@users.noreply.github.com>
2022-09-21 16:14:51 +0300

    Add files via upload

---
+++ vijener/vijener/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vijener
+{
+    class VigenereCipher
+    {
+        string alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦШЩЪЬЮЯ";
+
+        //Приравняване на дължината на ключа с тази на съобщението
+        private string CreateKey(string password, int length)
+        {
+            var pass = password;
+            while (pass.Length < length)
+            {
+                //докато дължината на ключа е по-малка от тази на съобщението добавяме буквите на ключа, за да получим равна дължина
+                pass += pass;
+            }
+            // премахваме излишните букви от ключа до дължината на съобщението
+            return pass.Substring(0, length);
+        }
+
+        public string SolveVigenere(string message, string password, bool operation)
+        {
+            var key = CreateKey(password, message.Length);
+            var solution = "";
+            var length = alphabet.Length;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                // взимаме индекса на буквата от съобщението
+                var letterIndex = alphabet.IndexOf(message[i]);
+                // взимаме индекса на буквата от ключа
+                var keyIndex = alphabet.IndexOf(key[i]);
+                if (letterIndex < 0)
+                {
+                    //Ако текстът не бъде намерен се добавя в оригинален вид
+                    solution += message[i].ToString();
+                }
+                else
+                {
+                    // записваме буквата на позицията която съотвества на:
+                    // (броят на буквите в азбуката + индексът на буквата от съобщениеето + индексът на буквата от ключа (- ако операцията е дешифриране)) % броят на буквите в азбуката
+                    solution += alphabet[(length + letterIndex + ((operation ? 1 : -1) * keyIndex)) % length].ToString();
+                }
+            }
+
+            if (solution == message)
+                Console.WriteLine("Моля използвайте българската азбука за съобщението и ключа");
+
+            return solution;
+        }
+    }
+}
---
+++ vijener/vijener/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace vijener
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            VigenereCipher cipher = new VigenereCipher();
+            Console.Write("Ввъведете текст, който да бъде шифриран: ");
+            var inputText = Console.ReadLine().ToUpper();
+            Console.Write("Въведете ключ за шифрирания текст: ");
+            var password = Console.ReadLine().ToUpper();
+            Console.WriteLine("Въведете индекса на желаната операция");
+            Console.WriteLine("1. Шифриране");
+            Console.WriteLine("2. Дешифриране");
+            var operaion = Console.ReadLine();
+            if (operaion == "1")
+                Console.WriteLine("Шифрирано съобщение: {0}", cipher.SolveVigenere(inputText, password, true));
+            else
+                if (operaion == "2")
+                Console.WriteLine("Дешифрирано съобщение: {0}", cipher.SolveVigenere(inputText, password, false));
+            else
+                Console.WriteLine("Избрали сте невалидна операция!");
+
+        }
+    }
+}
+
